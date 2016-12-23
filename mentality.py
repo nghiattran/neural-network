7fc96d13c77b1c400a272d6d14771f4161de0acd
Nghia Tran <nghiattran3@gmail.com>
2016-12-22 20:19:19 -0500

    test with pypi

---
+++ mentality.py
@@ -0,0 +1,5 @@
+__version__ = '0.0.1'
+__author__ = 'Nghia Tran'
+__author_email__ = 'nghiattran3@gmail.com'
+__github__ = 'https://github.com/nghiattran/mentality'
+__description__ = 'A random test lib'
---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+import mentality as package
+
+
+setup(
+    name=package.__name__,
+    packages=['lib'],
+    version=package.__version__,
+    py_modules=[package.__name__],
+    description = package.__description__,
+    author = package.__author__,
+    author_email = package.__author_email__,
+    url = package.__github__,
+    # download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1',
+    keywords = ['neural network', 'neural', 'network', 'perceptron'],
+    classifiers = [],
+)
